2c0f9bf7c7cdb401bfd6f9a29a4de07da340f05a
kmk <aksrl132@gmail.com>
2023-07-11 11:35:12 +0900

    1st

--- my_chatbot/test/model_intent_test.py
+++ my_chatbot/test/model_intent_test.py
@@ -8,7 +8,8 @@ p = Preprocess(word2index_dic='../train_tools/dict/chatbot_dict.bin',
                userdic='../utils/user_dic.tsv')
 
 intent = IntentModel(model_name='../model/intent/intent_model.h5', proprocess=p)
-query = "오늘 탕수육 주문 가능한가요?"
+#query = "오늘 탕수육 주문 가능한가요?"
+query = "강원, 매봉산 코스 찾아줘"
 predict = intent.predict_class(query)
 predict_label = intent.labels[predict]
 
